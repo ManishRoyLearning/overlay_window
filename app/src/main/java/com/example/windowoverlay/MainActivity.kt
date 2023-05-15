2be5fe45e9448ba2afc565047b46a5645abf6229
Manish <hrysocialmedia108@gmail.com>
2023-05-15 22:51:17 +0530

    Second Version with cross tik button

--- app/src/main/java/com/example/windowoverlay/MainActivity.kt
+++ app/src/main/java/com/example/windowoverlay/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.windowoverlay
 
+import android.app.ActivityManager
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,6 +13,8 @@ import android.provider.Settings
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var dialog: AlertDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,14 +35,51 @@ class MainActivity : AppCompatActivity() {
         }
 
         start_service.setOnClickListener {
-            val service = Intent(this,OverlayService::class.java)
+            val service = Intent(this@MainActivity,OverlayService::class.java)
             startService(service)
         }
 
         stop_service.setOnClickListener {
-            val service = Intent(this,OverlayService::class.java)
+            val service = Intent(this@MainActivity,OverlayService::class.java)
             stopService(service)
         }
 
+    }
+
+    private fun isServiceRunning(): Boolean{
+        var manger = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+
+        for (service in manger.getRunningServices(Int.MAX_VALUE)){
+
+            if (OverlayService::class.java.name == service.service.className)
+                return false
+        }
+        return false
+    }
+
+    private fun requestFloatingWindowPermission() {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setCancelable(true)
+        builder.setTitle("Screen Overlay Permission Needed")
+        builder.setMessage("Enable 'Display over the App' from settings")
+        builder.setPositiveButton("Open Settings", DialogInterface.OnClickListener { dialog, which ->
+
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivityForResult(intent, RESULT_OK)
+
+        })
+        dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun checkOverlayPermission(): Boolean{
+        return if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M){
+            Settings.canDrawOverlays(this)
+        }
+        else return true
     }
 }
--- app/src/main/java/com/example/windowoverlay/OverlayService.kt
+++ app/src/main/java/com/example/windowoverlay/OverlayService.kt
@@ -6,24 +6,37 @@ import android.content.Intent
 import android.graphics.PixelFormat
 import android.os.Build
 import android.os.IBinder
-import android.view.Gravity
-import android.view.MotionEvent
-import android.view.View
+import android.transition.AutoTransition
+import android.transition.TransitionManager
+import android.view.*
 import android.view.View.OnClickListener
-import android.view.View.OnTouchListener
-import android.view.WindowManager
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 
-class OverlayService : Service(),OnTouchListener, OnClickListener{
+class OverlayService : Service(), OnClickListener{
+    private lateinit var crossButton: ImageButton
+    private lateinit var tikButton: ImageButton
+    private lateinit var hiddenView: ConstraintLayout
+    private lateinit var cardView: CardView
+    private lateinit var icon: ImageView
+    private lateinit var processingIcon: TextView
     private var moving = false
     private var initialtouchY = 0.0f
     private var initialTouchX = 0.0f
     private var initialY = 0
     private var initialX = 0
+    private lateinit var floatView: ViewGroup
     private lateinit var params: WindowManager.LayoutParams
-    private lateinit var overLayButton: ImageButton
+//    private lateinit var overLayButton: ImageButton
     private lateinit var windowManager: WindowManager
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
 
     override fun onCreate() {
         super.onCreate()
@@ -32,13 +45,16 @@ class OverlayService : Service(),OnTouchListener, OnClickListener{
 
         windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
 
+        val inflater = baseContext.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
+        floatView = inflater.inflate(R.layout.floating_window,null) as ViewGroup
 
-        overLayButton = ImageButton(this)
-        overLayButton.setImageResource(R.drawable.circle_launcher)
-        overLayButton.background = null
-        overLayButton.setOnTouchListener(this)
-        overLayButton.setOnClickListener(this)
+        cardView = floatView.findViewById(R.id.base_cardview)
+        crossButton = floatView.findViewById(R.id.cross_button)
+        tikButton = floatView.findViewById(R.id.tik_button)
+        hiddenView = floatView.findViewById(R.id.hidden_view)
+        icon = floatView.findViewById(R.id.icon)
+        processingIcon = floatView.findViewById(R.id.heading)
 
         val layoutFlag = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
@@ -52,43 +68,77 @@ class OverlayService : Service(),OnTouchListener, OnClickListener{
             layoutFlag,
             WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
             PixelFormat.TRANSLUCENT
-        );
+        )
 
         params.gravity = Gravity.TOP or Gravity.START
 
         params.x = 0
         params.y = 100
 
-        windowManager.addView(overLayButton,params)
+        windowManager.addView(floatView,params)
+
+        icon.setOnClickListener {
+
+            if (hiddenView.visibility == View.VISIBLE) {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    TransitionManager.beginDelayedTransition(cardView, AutoTransition())
+                }
+                hiddenView.visibility = View.GONE
+            } else {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    TransitionManager.beginDelayedTransition(cardView, AutoTransition())
+                }
+                hiddenView.visibility = View.VISIBLE
+                processingIcon.visibility = View.VISIBLE
+            }
+        }
+
+        crossButton.setOnClickListener {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                TransitionManager.beginDelayedTransition(cardView, AutoTransition())
+            }
+            hiddenView.visibility = View.GONE
+            processingIcon.visibility = View.GONE
+        }
+
+        tikButton.setOnClickListener {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                TransitionManager.beginDelayedTransition(cardView, AutoTransition())
+            }
+            hiddenView.visibility = View.GONE
+            processingIcon.visibility = View.GONE
+        }
+
+        floatView.setOnTouchListener(object : View.OnTouchListener {
+
+            override fun onTouch(view: View?, event: MotionEvent?): Boolean {
+                view!!.performClick()
+
+                when(event!!.action){
+                    MotionEvent.ACTION_DOWN ->{
+                        initialX = params.x
+                        initialY = params.y
+                        initialTouchX = event.rawX
+                        initialtouchY = event.rawY
+                        moving = true
+                    }
+                    MotionEvent.ACTION_MOVE ->{
+                        params.x = initialX + (event.rawX - initialTouchX).toInt()
+                        params.y = initialY + (event.rawY - initialtouchY).toInt()
+                        windowManager.updateViewLayout(floatView,params)
+                    }
+                }
+                return true
+            }
+        })
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        windowManager.removeView(overLayButton)
-    }
-
-    override fun onBind(p0: Intent?): IBinder? {
-        return null
-    }
-
-    override fun onTouch(view: View?, event: MotionEvent?): Boolean {
-        view!!.performClick()
-
-        when(event!!.action){
-            MotionEvent.ACTION_DOWN ->{
-                initialX = params.x
-                initialY = params.y
-                initialTouchX = event.rawX
-                initialtouchY = event.rawY
-                moving = true
-            }
-            MotionEvent.ACTION_MOVE ->{
-                params.x = initialX + (event.rawX - initialTouchX).toInt()
-                params.y = initialY + (event.rawY - initialtouchY).toInt()
-                windowManager.updateViewLayout(overLayButton,params)
-            }
-        }
-        return true
+        windowManager.removeView(floatView)
     }
 
     override fun onClick(p0: View?) {
